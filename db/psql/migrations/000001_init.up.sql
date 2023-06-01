b60a34dc886d76da59d15fb64e124a8d44a81fa4
Thunder33345 <Thunder33345@users.noreply.github.com>
2023-06-01 23:40:03 +0800

    updated sql schema, standardized naming tables

--- db/psql/migrations/000001_init.up.sql
+++ db/psql/migrations/000001_init.up.sql
@@ -1,52 +1,59 @@
 BEGIN;
 
-CREATE TABLE authors
+CREATE TABLE author
 (
-    id           uuid NOT NULL PRIMARY KEY,
-    author       text NOT NULL UNIQUE CHECK (author <> ''),
-    created_at   timestamp DEFAULT current_timestamp,
-    last_updated timestamp DEFAULT current_timestamp
+    id          uuid        NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name text        NOT NULL UNIQUE CHECK (name <> ''),
+    created_at  timestamptz NOT NULL             DEFAULT now(),
+    updated_at  timestamptz NOT NULL             DEFAULT now()
 );
+-- Create index for author.created_at, as we will be using that for paging
+CREATE UNIQUE INDEX index_author ON author USING btree (created_at ASC);
 
-CREATE TABLE genres
+CREATE TABLE genre
 (
-    id           uuid NOT NULL PRIMARY KEY,
-    genre        text NOT NULL UNIQUE CHECK (genre <> ''),
-    created_at   timestamp DEFAULT current_time,
-    last_updated timestamp DEFAULT current_date
+    id         uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name       text NOT NULL UNIQUE CHECK (name <> ''),
+    created_at timestamptz               DEFAULT now(),
+    updated_at timestamptz               DEFAULT now()
 );
-CREATE TABLE books
-(
-    isbn         text PRIMARY KEY,
-    title        text    NOT NULL CHECK (title <> ''),
-    publish_year integer NOT NULL CHECK (publish_year > 0),
-    has_cover    boolean   DEFAULT false,
-    author_id    uuid    NOT NULL,
-    genre_id     uuid    NOT NULL,
-    last_updated timestamp DEFAULT current_timestamp,
-    created_at   timestamp DEFAULT current_timestamp,
-    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors (id) ON DELETE RESTRICT,
-    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE RESTRICT
-);
+CREATE UNIQUE INDEX index_genre ON genre USING btree (created_at ASC);
 
-CREATE TABLE users
+CREATE TABLE book
+(
+    isbn         text PRIMARY KEY NOT NULL CHECK (isbn <> ''),
+    title        text             NOT NULL CHECK (title <> ''),
+    publish_year integer          NOT NULL CHECK (publish_year > 0),
+    has_cover    boolean     DEFAULT false,
+    author_id    uuid             NOT NULL,
+    genre_id     uuid             NOT NULL,
+    updated_at   timestamptz DEFAULT now(),
+    created_at   timestamptz DEFAULT now(),
+    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE RESTRICT,
+    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE RESTRICT
+);
+CREATE UNIQUE INDEX index_book ON book USING btree (created_at ASC);
+
+-- Using user seems to be not recommended
+CREATE TABLE account
 (
     id            uuid        NOT NULL,
     name          text        NOT NULL CHECK (name <> ''),
     email         text UNIQUE NOT NULL CHECK (email <> ''),
     password_hash text        NOT NULL CHECK (password_hash <> ''),
-    is_admin      boolean   DEFAULT false,
-    created_at    timestamp DEFAULT current_date,
-    last_updated  timestamp DEFAULT current_date,
+    is_admin      boolean     DEFAULT false,
+    created_at    timestamptz DEFAULT now(),
+    updated_at    timestamptz DEFAULT now(),
     CONSTRAINT pk_users_id PRIMARY KEY (id)
 );
+CREATE UNIQUE INDEX index_account ON account USING btree (created_at ASC);
 
 CREATE TABLE sessions
 (
-    user_id    uuid NOT NULL,
+    account_id    uuid                    NOT NULL,
     token      text PRIMARY KEY UNIQUE NOT NULL CHECK (token <> ''),
-    created_at timestamp DEFAULT current_date,
-    CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    created_at timestamptz DEFAULT now(),
+    CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE
 );
 
 COMMIT;
